597e3e6c41ef843d969fbed6b7b974f0cff9d6ba
Andreas Höhne <info@webdesign-hoehne.de>
2020-05-29 11:44:04 +0200

    added script and simple description

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# sslvalid
+# sslvalid.sh
+simple script using curl to see if a ssl certificate is valid or not
---
+++ sslvalid.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+color_error="\e[41m"
+color_success="\e[42m"
+color_normal="\e[49m"
+color_option="-e"
+
+colorized="true"
+
+if [[ "$1" == "https://"* ]] ; then
+	#echo correct protocol
+	domain="$1"
+elif [[ "$1" == "http"* ]] ; then
+	#echo "correct protocol"
+	domain="$(echo "$1" | sed 's/http:\/\//https:\/\//' -)"
+	#echo "corrected domain: $domain"
+	#exit
+else
+	domain="https://$1"
+fi
+
+if [ "$colorized" != "true" ] ; then
+	color_error=""
+	color_success=""
+	color_normal=""
+	color_option=""
+fi
+
+if [ "$(curl "$domain" -I 2> /dev/null)" ] ; then 
+	echo ${color_option} ${color_success}valid${color_normal}
+else
+	echo ${color_option} ${color_error}invalid${color_normal}
+fi
