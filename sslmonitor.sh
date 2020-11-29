b75552b47f65eaa7b61d1899315f1dfd4cafc7d8
Andreas Höhne <info@webdesign-hoehne.de>
2020-11-29 20:42:30 +0100

    configure option for monitor interval

--- sslmonitor.sh
+++ sslmonitor.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #this script is designed to just be used with domain names
 workdir="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+interval="10" # interval in seconds
 while [ 1 ] ; do
 	date
 	"${workdir}/sslvalid.sh" "$1"
 	"${workdir}/sslvalid.sh" "www.${1}"
-	sleep 300
+	sleep 10
 done
