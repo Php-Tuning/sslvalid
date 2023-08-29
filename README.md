82695dc5efd454ac3d8e5639b58ae7673111b06b
Andreas Höhne <info@webdesign-hoehne.de>
2023-08-29 15:50:57 +0200

    fixed last test issue and implemented badge

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
+[![CI Shellcheck](https://github.com/Php-Tuning/sslvalid/actions/workflows/shellcheck.yml/badge.svg)](https://github.com/Php-Tuning/sslvalid/actions/workflows/shellcheck.yml)
 # sslvalid.sh
 simple script using curl to see if a ssl certificate is valid or not
--- sslmonitor.sh
+++ sslmonitor.sh
@@ -2,7 +2,7 @@
 #this script is designed to just be used with domain names
 workdir="$( cd "$(dirname "$0")" || exit >/dev/null 2>&1 ; pwd -P )"
 interval="10" # interval in seconds
-while [ 1 ] ; do
+while true ; do
 	date
 	"${workdir}/sslvalid.sh" "$1"
 	"${workdir}/sslvalid.sh" "www.${1}"
